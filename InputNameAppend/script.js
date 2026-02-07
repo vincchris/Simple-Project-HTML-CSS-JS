864de9578c0193805bc632b0146064ec95ab6896
Vincent Chriss <vchristianthen@gmail.com>
2026-02-07 21:12:39 +0700

    Update Type 4 InputNameAppend Folder

--- InputNameAppend/script.js
+++ InputNameAppend/script.js
@@ -3,5 +3,5 @@ let sendBtn = document.getElementById('sendBtn')
 let txtResult = document.getElementById('txtResult')
 
 sendBtn.addEventListener('click', () => {
-  inpTxt.textContent = txtResult
+  txtResult.textContent = "Hello " + inpTxt.value;
 })
