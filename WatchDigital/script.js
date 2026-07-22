1162535b239762eb16dc65d5558760d1f49ed7f7
Vincent Chriss <vchristianthen@gmail.com>
2026-07-22 22:18:22 +0700

    Update Progress WatchDigital

---
+++ WatchDigital/script.js
@@ -0,0 +1,23 @@
+function clock() {
+  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+  const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+  let today = new Date()
+
+  document.getElementById("Date").innerHTML = (dayNames[today.getDay()] + " " + today.getDate() + " " + monthNames[today.getMonth()] + " " + today.getFullYear())
+
+  let h = today.getHours()
+  let m = today.getMinutes()
+  let s = today.getSeconds()
+  let day = h<11 ? "AM" : "PM"
+
+  h = h<10 ? '0'+h: h
+  m = m<10 ? '0'+m: m
+  s = s<10 ? '0'+s: s
+
+  document.getElementById("hours").innerHTML = h
+  document.getElementById("minutes").innerHTML = m
+  document.getElementById("seconds").innerHTML = s
+
+} let inter = setInterval(clock, 400)
